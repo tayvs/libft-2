e28766bc734b065bd374efd9d270e76d8a790396
tayvs <xakDeltaForseXYZ@gmail.com>
2018-12-05 15:42:52 +0200

    Update ft_strnequ.c
    fix seq fault

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -14,7 +14,9 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	if (ft_strncmp(s1, s2, n) == 0)
+	if (!s1 xor !s2)
+		return (0);
+	else if ((!s1 && !s2) || ft_strncmp(s1, s2, n) == 0)
 		return (1);
 	else
 		return (0);
